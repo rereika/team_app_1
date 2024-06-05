0ea73038c15eab403d75a33bee4b58962012fff7
tomoya <tomoyae1007.xperia26@gmail.com>
2024-06-05 11:27:43 +0900

    カレンダー動的生成を形だけ実装、これから調整

--- calendar/script.js
+++ calendar/script.js
@@ -12,6 +12,125 @@ document.addEventListener("DOMContentLoaded", () => {
     localStorage.setItem("monthlyGoal", goalInput.value);
   });
 
+  //カレンダー自動生成部分、左上の月も連動する
+  const today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth();
+
+  function getCalendarHead() {
+    const dates = [];
+    const d = new Date(year, month, 0).getDate();
+    const n = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < n; i++) {
+      dates.unshift({
+        date: d - i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+    return dates;
+  }
+
+  function getCalendarBody() {
+    const dates = [];
+    const lastDate = new Date(year, month + 1, 0).getDate();
+
+    for (let i = 1; i <= lastDate; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: false,
+      });
+    }
+
+    if (year === today.getFullYear() && month === today.getMonth()) {
+      dates[today.getDate() - 1].isToday = true;
+    }
+
+    return dates;
+  }
+
+  function getCalendarTail() {
+    const dates = [];
+    const lastDay = new Date(year, month + 1, 0).getDay();
+    for (let i = 1; i < 7 - lastDay; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: true
+      });
+    }
+    return dates;
+  }
+
+  function clearCalendar() {
+    const tbody = document.querySelector('.calendar tbody');
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+  }
+
+  function renderMonth() {
+    const title = month + 1;
+    document.querySelector('.goal-time-container span').textContent = title + '月';
+  }
+
+  function renderWeeks() {
+    const dates = [
+      ...getCalendarHead(),
+      ...getCalendarBody(),
+      ...getCalendarTail()
+    ];
+    const weeks = [];
+    const weeksCount = dates.length / 7;
+    for (let i = 0; i < weeksCount; i++) {
+      weeks.push(dates.splice(0, 7));
+    }
+
+    weeks.forEach((week) => {
+      const tr = document.createElement('tr');
+      week.forEach((date) => {
+        const td = document.createElement(('td'));
+        td.textContent = date.date;
+        if (date.isToday) {
+          td.classList.add('today');
+        }
+        if (date.isDisabled) {
+          td.classList.add('disabled');
+        }
+        tr.appendChild(td);
+      });
+      document.querySelector('.calendar tbody').appendChild(tr);
+    });
+  }
+
+  function createCalendar() {
+    clearCalendar();
+    renderMonth();
+    renderWeeks();
+  }
+
+  document.querySelector('#prev').addEventListener('click', () => {
+    month--;
+    if (month < 0) {
+      year--;
+      month = 11;
+    }
+    createCalendar();
+  });
+
+  document.querySelector('#next').addEventListener('click', () => {
+    month++;
+    if (month > 11) {
+      year++;
+      month = 0;
+    }
+    createCalendar();
+  });
+
+  // createCalendar();
+
   // モーダルウィンドウの要素を取得
   const modal = document.getElementById("myModal");
   const modalDate = document.getElementById("modal-date");
@@ -23,7 +142,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const editModalButton = document.getElementById("edit-modal");
 
   // カレンダーの日付セルをクリックしたときの処理
-  document.querySelectorAll("tbody td").forEach(cell => {
+  document.querySelectorAll(".calendar tbody td").forEach(cell => {
     cell.addEventListener("click", () => {
       modalDate.textContent = `${cell.firstElementChild.textContent}日`;
 
